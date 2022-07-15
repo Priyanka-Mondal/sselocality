83e91869495277e9acdb51131403472f52ccaa5e
Priyanka Mondal <priyanka02010@gmail.com>
2022-07-14 21:53:45 -0700

    bitonic sort started

--- bitonic.cpp
+++ bitonic.cpp
@@ -1,145 +1,87 @@
 #include<bits/stdc++.h>
-#include <algorithm>
-#include <functional>
-#include <array>
-#include <iostream>
- 
-
 using namespace std;
 
-
-vector<int> memAcc;
-void compAndSwap(int a[], int i, int j, int dir)
+/*The parameter dir indicates the sorting direction, ASCENDING
+or DESCENDING; if (a[i] > a[j]) agrees with the direction,
+then a[i] and a[j] are interchanged.*/
+void compAndSwap(int a[], int i, int j)
 {
-	if (dir==(a[i]>a[j]))
+	if ((a[i]>a[j]))
 		swap(a[i],a[j]);
 }
-void bitonicMerge(int a[], int low, int cnt, int dir, int N)
+
+/*It recursively sorts a bitonic sequence in ascending order,
+if dir = 1, and in descending order otherwise (means dir=0).
+The sequence to be sorted starts at index position low,
+the parameter cnt is the number of elements to be sorted.*/
+void bitonicMerge(int a[], int low, int cnt)
 {
 	if (cnt>1)
 	{
 		int k = cnt/2;
 		for (int i=low; i<low+k; i++)
 		{
-			compAndSwap(a, i, i+k, dir);
-			memAcc.push_back(i);
-			memAcc.push_back(i+k);
+			compAndSwap(a, i, i+k);
+			cout <<i<<" "<<i+k<<endl;
 		}
-		bitonicMerge(a, low, k, dir);
-		bitonicMerge(a, low+k, k, dir);
+		bitonicMerge(a, low, k);
+		bitonicMerge(a, low+k, k);
 	}
 }
-void bitonicSort(int a[],int low, int cnt, int dir)
+
+void bitMerge(int a[], int low, int cnt)
+{
+	if (cnt>1)
+	{
+		int k = cnt/2;
+		for (int i=low, j = low+cnt-1; i<low+k,j>=low+k; i++,j--)
+		{
+			compAndSwap(a, i, j);
+			cout <<i<<" "<<j<<endl;
+		}
+		bitonicMerge(a, low, k);
+		bitonicMerge(a, low+k, k);
+	}
+}
+
+/* This function first produces a bitonic sequence by recursively
+	sorting its two halves in opposite sorting orders, and then
+	calls bitonicMerge to make them in the same order */
+void bitonicSort(int a[],int low, int cnt)
 {
 	if (cnt>1)
 	{
 		int k = cnt/2;
 
 		// sort in ascending order since dir here is 1
-		bitonicSort(a, low, k, 1);
+		bitonicSort(a, low, k);
 
 		// sort in descending order since dir here is 0
-		bitonicSort(a, low+k, k, 0);
+		bitonicSort(a, low+k, k);
 
 		// Will merge whole sequence in ascending order
 		// since dir=1.
-		bitonicMerge(a,low, cnt, dir);
+		bitMerge(a,low, cnt);
 	}
 }
+
 void sort(int a[], int N, int up)
 {
-	bitonicSort(a,0, N, up);
-}
-
-// Driver code
-vector<int> getSeq(int count, int step)
-{
-	int start = count*step;
-	cout <<"count:"<<count<<"step:"<<step<<" start:"<<start<<" "<<memAcc[start]<<endl;
-	vector<int> res;
-	for(int i = start; i<start+step; i++)
-	{
-		res.push_back(memAcc[i]);
-		cout <<"("<<memAcc[i]<<")";
-	}
-	cout <<endl;
-	return res;
-}
-
-void memseq(int a[] , int n)
-{
-for (int k = 2; k <= n; k= k*2)
-{ 
-        for (int j = k/2; j > 0; j /= 2)
-		{ // j is halved at every iteration, with truncation of fractional parts
-            for (int i = 0; i < n; i++)
-			{
-                int l = i^j; // in C-like languages this is "i ^ j"
-                if (l > i)
-				{
-						memAcc.push_back(i);
-						memAcc.push_back(l);
-                    if ((((i & k) == 0) && (a[i] > a[l])) || (((i&k) != 0) && (a[i] < a[l])))
-					{
-						int temp = a[i];
-						a[i]=a[l];
-						a[l]=temp;
-					}
-				}
-			}
-		}
-}
+	bitonicSort(a,0, N);
 }
 
 int main()
 {
-	int p[]= {3, 7, 4, 11, 6, 0, 5, 99};
-	int N = sizeof(p)/sizeof(p[0]);
-	int a[10];
-	memset(a,0,N);
+	int a[]= {3, 7, 4, 8, 6, 2, 1, 5, 9,13,12,16,14,10,15,11, 17,18,19,20,31,32,30,28,26,29,21,25,22,24,23,27
+					};
+	int N = sizeof(a)/sizeof(a[0]);
 
 	int up = 1; // means sort in ascending order
-	//sort(a, N, up);
-	memseq(a,N);
-	int step = 4;
+	sort(a, N, up);
 
-	for(int i=0; i<memAcc.size(); i++)
-		cout <<memAcc[i] <<" ";
-	cout <<endl;
-	
-	int totmemAcc = memAcc.size();
-	int totStep = ceil((float)totmemAcc	/(float)step);
-	cout <<"tptStep:"<<totStep<<endl;
-	for(int count = 0; count<totStep; count++)
-	{
-		cout<<"count:"<<count<<endl;
-		vector<int> curMem;
-		curMem = getSeq(count, step);
-		cout <<"size of surMem:"<<curMem.size()<<endl;
-		vector<int> elToSort;
-		std::sort(curMem.begin(), curMem.end(), [](int a, int b) {return a < b;});
-		for(auto el : curMem)
-		{
-			cout <<"["<<p[el]<<"]";
-			elToSort.push_back(p[el]);
-		}
-		cout <<endl;
-		std::sort(elToSort.begin(), elToSort.end(), [](int a, int b) {return a < b;});
-		cout <<endl;
-		int cnt = 0;
-		for(int i =0; i<curMem.size(); i++)
-		{
-			p[curMem[i]] = elToSort[cnt];
-			cnt++;
-		}
-		for(int i =0; i<curMem.size(); i++)
-		{
-			cout<<"{"<<p[curMem[i]]<<"}";
-		}
-		cout <<endl;
-	}
+	printf("Sorted array: \n");
 	for (int i=0; i<N; i++)
-		printf("%d ", p[i]);
-
+		printf("%d ", a[i]);
 	return 0;
 }
+
