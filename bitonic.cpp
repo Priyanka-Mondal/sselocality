1e86986a3f7f6b36a4a110f6494c240f6293ffb8
Priyanka Mondal <priyanka02010@gmail.com>
2022-07-14 22:15:46 -0700

    deamortization of bitonic sort started

--- bitonic.cpp
+++ bitonic.cpp
@@ -9,7 +9,7 @@ void compAndSwap(int a[], int i, int j)
 	if ((a[i]>a[j]))
 		swap(a[i],a[j]);
 }
-
+vector<int> memseq;
 /*It recursively sorts a bitonic sequence in ascending order,
 if dir = 1, and in descending order otherwise (means dir=0).
 The sequence to be sorted starts at index position low,
@@ -22,7 +22,9 @@ void bitonicMerge(int a[], int low, int cnt)
 		for (int i=low; i<low+k; i++)
 		{
 			compAndSwap(a, i, i+k);
-			cout <<i<<" "<<i+k<<endl;
+			//cout <<i<<" "<<i+k<<endl;
+			memseq.push_back(i);
+			memseq.push_back(i+k);
 		}
 		bitonicMerge(a, low, k);
 		bitonicMerge(a, low+k, k);
@@ -37,7 +39,9 @@ void bitMerge(int a[], int low, int cnt)
 		for (int i=low, j = low+cnt-1; i<low+k,j>=low+k; i++,j--)
 		{
 			compAndSwap(a, i, j);
-			cout <<i<<" "<<j<<endl;
+			//cout <<i<<" "<<j<<endl;
+			memseq.push_back(i);
+			memseq.push_back(j);
 		}
 		bitonicMerge(a, low, k);
 		bitonicMerge(a, low+k, k);
@@ -70,10 +74,31 @@ void sort(int a[], int N, int up)
 	bitonicSort(a,0, N);
 }
 
+vector<int> getSeq(int count, int step)
+{
+	int start = count*step;
+	cout <<"count:"<<count<<"step:"<<step<<" start:"<<start<<" "<<memseq[start]<<endl;
+	vector<int> res;
+	for(int i = start; i<start+step; i++)
+	{
+		res.push_back(memseq[i]);
+		cout <<"("<<memseq[i]<<")";
+	}
+	cout <<endl;
+	return res;
+}
+
+
+void deamortizeBitonicSort(int step, int a[], int N)
+{
+}
+
 int main()
 {
-	int a[]= {3, 7, 4, 8, 6, 2, 1, 5, 9,13,12,16,14,10,15,11, 17,18,19,20,31,32,30,28,26,29,21,25,22,24,23,27
-					};
+	int p[]= {3, 7, 4, 8, 6, 2, 1, 5, 9,13,12,16,14,10,15,11, 17,18,19,20,31,32,30,28,26,29,21,25,22,24,23,27};
+	int a[32];
+	int step = 4;
+	memset(a,0,32);
 	int N = sizeof(a)/sizeof(a[0]);
 
 	int up = 1; // means sort in ascending order
@@ -82,6 +107,40 @@ int main()
 	printf("Sorted array: \n");
 	for (int i=0; i<N; i++)
 		printf("%d ", a[i]);
+	deamortizeBitonicSort(step,a,N);
+	int totmemseq = memseq.size();
+	int totStep = ceil((float)totmemseq	/(float)step);
+	for(int count = 0; count<totStep; count++)
+	{
+		cout<<"count:"<<count<<endl;
+		vector<int> curMem;
+		curMem = getSeq(count, step);
+		cout <<"size of surMem:"<<curMem.size()<<endl;
+		vector<int> elToSort;
+		std::sort(curMem.begin(), curMem.end(), [](int a, int b) {return a < b;});
+		for(auto el : curMem)
+		{
+			cout <<"["<<p[el]<<"]";
+			elToSort.push_back(p[el]);
+		}
+		cout <<endl;
+		std::sort(elToSort.begin(), elToSort.end(), [](int a, int b) {return a < b;});
+		cout <<endl;
+		int cnt = 0;
+		for(int i =0; i<curMem.size(); i++)
+		{
+			p[curMem[i]] = elToSort[cnt];
+			cnt++;
+		}
+		for(int i =0; i<curMem.size(); i++)
+		{
+			cout<<"{"<<p[curMem[i]]<<"}";
+		}
+		cout <<endl;
+	}
+	for (int i=0; i<N; i++)
+		printf("%d ", p[i]);
+
 	return 0;
 }
 
